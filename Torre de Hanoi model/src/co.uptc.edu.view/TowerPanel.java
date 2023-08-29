3ef95d4d52773f5e322f39593c0800437167819e
WilliamC111 <110690003+WilliamC111@users.noreply.github.com>
2023-08-29 15:08:52 -0500

    Torres en el panel

--- Torre de Hanoi model/src/co.uptc.edu.view/TowerPanel.java
+++ Torre de Hanoi model/src/co.uptc.edu.view/TowerPanel.java
@@ -1,15 +1,45 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-class TowerPanel extends JPanel {
+public class TowerPanel extends JPanel {
+
     private Tower tower;
+    private Disk draggedDisk;
+    private JLabel backgroundLabel;
+
     public TowerPanel(Tower tower) {
         this.tower = tower;
         setPreferredSize(new Dimension(150, 400));
-    }
+        setLayout(new BorderLayout());
 
+        backgroundLabel = new JLabel(new ImageIcon("Fondo.jpg"));
+        add(backgroundLabel);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (tower.getDisks().isEmpty()) {
+                    return;
+                }
+                draggedDisk = tower.getTopDisk();
+                tower.removeDisk();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (draggedDisk != null) {
+                    TowerPanel targetPanel = (TowerPanel) e.getComponent();
+                    targetPanel.dropDisk(draggedDisk);
+                    draggedDisk = null;
+                }
+            }
+        });
+    }
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -20,6 +50,14 @@ class TowerPanel extends JPanel {
             g.setColor(Color.BLUE);
             g.fillRect(x - width / 2, y, width, 15);
             y -= 20;
+        }
+    }
+
+    public void dropDisk(Disk disk) {
+        if (tower.addDisk(disk)) {
+            repaint();
+        } else {
+            tower.addDisk(draggedDisk);
         }
     }
 }
